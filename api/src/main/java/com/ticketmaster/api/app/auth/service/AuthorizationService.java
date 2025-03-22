02b12a72091b30e69168e704af7ab9a270291308
Paulo Prado <paulopraddo011@gmail.com>
2025-03-22 13:03:33 -0300

    create authservice

---
+++ api/src/main/java/com/ticketmaster/api/app/auth/service/AuthorizationService.java
@@ -0,0 +1,22 @@
+package com.ticketmaster.api.app.auth.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.ticketmaster.api.domain.auth.repository.UserRepository;
+
+@Service
+public class AuthorizationService implements UserDetailsService {
+
+    @Autowired
+    UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return this.repository.findByUsername(username);
+    }
+
+}
