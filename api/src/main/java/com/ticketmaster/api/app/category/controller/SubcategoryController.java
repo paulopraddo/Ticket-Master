ad6abd56e09fa8d39e4bc6ed86978711a5cc8202
Paulo Prado <paulopraddo011@gmail.com>
2025-03-24 20:22:39 -0300

    get subcategory

--- api/src/main/java/com/ticketmaster/api/app/category/controller/SubcategoryController.java
+++ api/src/main/java/com/ticketmaster/api/app/category/controller/SubcategoryController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,6 +32,11 @@ public class SubcategoryController {
         this.subcategorySevice.uploadCategory(dto);
 
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{name}")
+    public ResponseEntity<GetSubcategoryDTO> getSubcategory(@PathVariable String name) {
+        return ResponseEntity.ok().body(this.subcategorySevice.getSubcategory(name));
     }
 
     @GetMapping
