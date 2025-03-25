baf957e723f4db217575220c13d5729361401605
Paulo Prado <paulopraddo011@gmail.com>
2025-03-25 18:16:55 -0300

    feat getListOfEvents

--- api/src/main/java/com/ticketmaster/api/app/event/controller/EventController.java
+++ api/src/main/java/com/ticketmaster/api/app/event/controller/EventController.java
@@ -1,12 +1,16 @@
 package com.ticketmaster.api.app.event.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ticketmaster.api.app.event.dtos.GetEventResponseDTO;
 import com.ticketmaster.api.app.event.dtos.UploadEventRequestDTO;
 import com.ticketmaster.api.app.event.service.EventService;
 
@@ -27,5 +31,13 @@ public class EventController {
         this.eventService.uploadEvent(dto);
 
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<GetEventResponseDTO>> getListOfEvents() {
+
+        List<GetEventResponseDTO> listOfDtos = this.eventService.getListOfEvents();
+
+        return ResponseEntity.ok().body(listOfDtos);
     }
 }
